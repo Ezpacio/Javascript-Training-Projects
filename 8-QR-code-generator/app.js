d6d279132406c6fa1cc31322627987af460c38af
Samet Gokkaya <sametgokkayax@gmail.com>
2024-01-10 14:15:47 +0300

    created qr-code-generator project

---
+++ 8-QR-code-generator/app.js
@@ -0,0 +1,14 @@
+const qrContainer = document.querySelector('.qr-code-box');
+const button = document.getElementById('btn');
+const qrInput = document.querySelector('input[type="text"]');
+const qrImage = document.querySelector('.qr-code img')
+
+button.addEventListener('click', () => {
+    const inputValue = qrInput.value;
+    if(inputValue.trim() === ""){return}else{inputValue.trim()};
+
+    qrImage.src = `https://api.qrserver.com/v1/create-qr-code/?size=160x160&data=${inputValue}`
+    qrImage.addEventListener('load', () =>{
+        qrContainer.classList.add('showQr');
+    });
+});
