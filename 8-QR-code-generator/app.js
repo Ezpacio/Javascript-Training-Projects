bd1dc903d0b4793cd2d775fe173b0c2e9ae5bae3
Samet Gokkaya <sametgokkayax@gmail.com>
2024-01-10 16:09:34 +0300

    some changes for crome extension

--- 8-QR-code-generator/app.js
+++ 8-QR-code-generator/app.js
@@ -1,7 +1,9 @@
 const qrContainer = document.querySelector('.qr-code-box');
 const button = document.getElementById('btn');
 const qrInput = document.querySelector('input[type="text"]');
-const qrImage = document.querySelector('.qr-code img')
+const qrImage = document.querySelector('.qr-code img');
+const footerLinks = document.querySelector('footer');
+console.log(footerLinks);
 
 button.addEventListener('click', () => {
     const inputValue = qrInput.value;
@@ -10,5 +12,6 @@ button.addEventListener('click', () => {
     qrImage.src = `https://api.qrserver.com/v1/create-qr-code/?size=160x160&data=${inputValue}`
     qrImage.addEventListener('load', () =>{
         qrContainer.classList.add('showQr');
+        footerLinks.classList.add('hidden')
     });
 });
